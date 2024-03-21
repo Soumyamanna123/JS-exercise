d2ba4b264847baef2d4fae38e4e9a7b30bd9575d
Soumyamanna123 <soumyamanna729@gmail.com>
2024-03-21 12:10:18 +0530

    changes

--- option-chaining.js
+++ option-chaining.js
@@ -1,16 +1,11 @@
-console.log ("fgbf");
-const invalidindex=(arr,i)=>{
-    if(i<0 || i>arr.length){
-        throw "Invalid index,can't access"
-    }
-    else{
-        console.log(arr[i])
-    }
-}
-try{
-    let array=[3,4,5,6]
-    invalidindex(array,5);
-}
-catch(err){
-    console.log("Q7=> Error: "+err)
-}
+const user ={
+    name: "soumya",
+    age: 26,
+    address: {
+        street: 24,
+        location: "kolkata",
+        pin : 700056,
+        g: null ,
+    },
+};
+console.log(user.address?. g);
