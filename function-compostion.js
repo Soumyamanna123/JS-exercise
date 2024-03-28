b721ef586c6d978c968e6f7a4597da4b529db6b0
Soumyamanna123 <soumyamanna729@gmail.com>
2024-03-28 13:29:15 +0530

    change

--- function-compostion.js
+++ function-compostion.js
@@ -1,8 +1,26 @@
-const add5 = (x) => x + 5;
-const multiplyBy3 = (x) => x * 3;
-const subtract10 = (x) => x - 10;
+// Function to double a number
+function double(x) {
+    return x * 2;
+}
 
-const composedFunction = compose(subtract10, multiplyBy3, add5);
-const result = composedFunction(7);
+// Function to add 3 to a number
+function addThree(x) {
+    return x + 3;
+}
 
-console.log(result);
+// Function to square a number
+function square(x) {
+    return x * x;
+}
+// Function composition example
+function compose(...fns) {
+    return function(x) {
+        return fns.reduceRight((acc, fn) => fn(acc), x);
+    };
+}
+
+// Create the composed function
+const composedFunction = compose(double, addThree, square);
+
+// Test the composed function
+console.log(composedFunction(2)); // Output: 22
