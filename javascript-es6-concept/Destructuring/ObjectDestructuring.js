cebab297f5ca0179041610a1ef643f9f079eee65
Soumyamanna123 <soumyamanna729@gmail.com>
2024-08-12 14:38:08 +0530

    node

---
+++ javascript-es6-concept/Destructuring/ObjectDestructuring.js
@@ -0,0 +1,15 @@
+// why we need destructuring?
+
+// ->
+
+
+
+const user ={
+    id: 37,
+    name: "soumya",
+    age: 20,
+
+}
+const { name, id }= user;
+console.log (name, id)
+console.log("a")
